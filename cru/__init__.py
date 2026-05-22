009e797150839ee91220593a37b578504bc28701
skelmis <skelmis.craft@gmail.com>
2026-05-23 00:09:52 +1200

    chore: versioning

---
+++ cru/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+
+import logging
+from collections import namedtuple
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="beta", serial=0)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
